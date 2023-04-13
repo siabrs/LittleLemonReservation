6bcc4e770417efa6f3a45109f0a1a9d4972a70e1
Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>
2023-04-13 12:45:21 +0300

    Add initial project
    Sourses provided as a base for a course project
    
    Signed-off-by: Oleksandra Siabrenko <oleksandra.siabrenko@gmail.com>

---
+++ StartingProject/LitttleLemonLogo.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
+
+
